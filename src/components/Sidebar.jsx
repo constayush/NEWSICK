04b48212faa0a71745644354cbb90283536722dc
constayush <aayush@mail.com>
2024-04-17 12:34:51 +0530

    ui changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sidebar() {
   return (
-<div className='Sidebar w-[28rem] h-full bg-black py-5 px-5'>
+<div className='Sidebar w-full h-[6.5rem]  md:w-[28rem] md:h-full bg-black py-5 px-5'>
 
 
 <div className='w-full h-full greygra2 rounded-lg'>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,12 @@ function Home() {
     return (<>
 
 
-        <div className='Home flex w-screen justify-center items-center'>
+        <div className='Home flex md:flex-row flex-col w-screen justify-center items-center'>
 
 
             <Sidebar />
 
-            <div className=' w-full h-full pr-5  py-5 bg-black'>
+            <div className=' w-full h-full pr-5 pl-5 md:pl-0 py-5 bg-black'>
 
 
                 <div className='w-full h-full greygra rounded-lg'></div>
@@ -21,7 +21,7 @@ function Home() {
 
         </div>
 
-        <div className='playerNav w-screen h-[4.5rem]'></div>
+        <div className='playerNav w-screen h-[4.5rem] bg-[#030303]'></div>
 
 
     </>)
