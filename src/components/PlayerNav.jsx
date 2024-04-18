c9a728e7e6311197c8db2dc63221c3008eb129a3
constayush <aayush@mail.com>
2024-04-19 01:21:33 +0530

    input ranges added

--- src/components/PlayerNav.jsx
+++ src/components/PlayerNav.jsx
@@ -1,21 +1,63 @@
 import React from 'react'
-import playIcon from  '../../public/asset/playbtn.svg'
-import nextIcon from  '../../public/asset/nextbtn.svg'
-import loopIcon from  '../../public/asset/loopbtn.svg'
-import repeatIcon from  '../../public/asset/repeatbtn.svg'
-import shuffleIcon from  '../../public/asset/shufflebtn.svg'
+import playIcon from '../../public/asset/playbtn.svg'
+import nextIcon from '../../public/asset/nextbtn.svg'
+import loopIcon from '../../public/asset/loopbtn.svg'
+import repeatIcon from '../../public/asset/repeatbtn.svg'
+import shuffleIcon from '../../public/asset/shufflebtn.svg'
 import previousIcon from '../../public/asset/prvbtn.svg'
 
 function PlayerNav() {
   return (
-    <div className='playerNav w-screen h-[4.5rem] bg-[#030303] flex justify-center items-center gap-[2rem]'>
+    <div className='playerNav w-screen  h-[4.5rem] greygra2 flex justify-evenly items-center p-[2rem]'>
 
 
-<button><img className='w-[40px] h-[40px]' src={shuffleIcon} /></button>
-<button><img className='w-[40px] h-[40px]' src={previousIcon} /></button>
-<button><img className='w-[40px] h-[40px]' src={playIcon} /></button>
-<button><img className='w-[40px] h-[40px]' src={nextIcon} /></button>
-<button><img className='w-[40px] h-[40px]' src={repeatIcon} /></button>
+
+
+      <div className='PlayerInfo flex justify-center items-center w-[25%] gap-6'>
+
+
+        <img className='songCover h-[40px] w-[40px] border-white border-[1px] ' />
+
+        <div className='songDetails text-[#c5c3c3]'>
+          <h1 className='songName font-bold'>Song</h1>
+          <p className='songArtist'>Artist</p>
+        </div>
+
+
+      </div>
+
+
+
+      <div className='flex  flex-col justify-center items-center  w-[50%] gap-4'>
+
+        <div className='flex justify-center items-center gap-5'>
+          <button> <img className='w-[28px] h-[28px]' src={shuffleIcon} />      </button>
+          <button> <img className='w-[28px] h-[28px]' src={previousIcon} />     </button>
+          <button> <img className='w-[28px] h-[28px]' src={playIcon} />         </button>
+          <button> <img className='w-[28px] h-[28px]' src={nextIcon} />         </button>
+          <button> <img className='w-[28px] h-[28px]' src={repeatIcon} />       </button>
+        </div>
+
+
+
+
+
+
+
+
+        <input type='range' className='h-[1px] w-full'></input>
+      </div>
+
+
+
+
+      <div className='volumeControlContainer w-[25%] flex justify-center items-center'>
+
+        <input type='range'></input>
+
+      </div>
+
+
 
 
 
