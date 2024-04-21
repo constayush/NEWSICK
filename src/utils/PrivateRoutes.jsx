33e2650079724a4bc4b3c34223cbf64707c88e3f
constayush <aayush@mail.com>
2024-04-21 23:19:16 +0530

    PRIVATE ROUTE ADDED

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 import { getTokenFromResponse } from '../spotify'
+import { useEffect, useState } from 'react'
+
 function PrivateRoutes() {
 
-    
-  const token = getTokenFromResponse().access_token
-  return (
+  let token = null;
 
-token ? <Outlet/> : <Navigate to='login' />
+  token = getTokenFromResponse().access_token
 
-  )
+  return  token ? <Outlet /> : <Navigate to='login' />
+ 
 }
 
 export default PrivateRoutes
+
+
+
+
